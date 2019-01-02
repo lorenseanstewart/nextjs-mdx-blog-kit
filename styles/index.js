2d6782fcd3aed8f32c5865e04d9ff918fdec63dd
lorenseanstewart <l.s.stewart111@gmail.com>
2019-01-02 07:22:33 -0800

    added line height for text nodes

--- styles/index.js
+++ styles/index.js
@@ -123,6 +123,7 @@ li,
 ul,
 ol {
     font-size: 18px;
+    line-height: 24px;
     word-break: break-word /* This prevents a bug with the reading progress bar. Leave this style here unless you are removing the reading progress bar. */
 }
 
